8f28312056731bd18a71c818b8758dd189a038f3
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-07-10 14:23:12 -0400

    initial commit

---
+++ ArtistLookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtistLookup
+//
+//  Created by Ying Quan Tan on 7/10/15.
+//  Copyright © 2015 Intrepid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
