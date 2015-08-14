9d159aa1b674dffad2d6ddf8b06d27bbcc6beec4
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-08-14 16:47:46 -0400

    fixed bugs in fast capture

--- ArtistLookup/FastCapture.swift
+++ ArtistLookup/FastCapture.swift
@@ -14,19 +14,17 @@ import CoreGraphics
 let defaultError = NSError(domain: "", code: 0, userInfo: nil)
 
 protocol IPFastCaptureDelegate {
-    func fastCaptureDidCaptureImage(image: UIImage, fastCapture: IPFastCapture)
+    func fastCaptureDidCaptureImage(image: UIImage)
 }
 
 /// Class that quickly captures outputs of an AVCaptureSession
 class IPFastCapture: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, G8TesseractDelegate {
 
     let captureSession = AVCaptureSession()
-    let tesseract: G8Tesseract
     var delegate: IPFastCaptureDelegate?
 
     override init() {
         output = AVCaptureVideoDataOutput()
-        tesseract = G8Tesseract(language: "eng")
 
         super.init()
 
@@ -74,7 +72,10 @@ class IPFastCapture: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, G8T
         fromConnection connection: AVCaptureConnection!) {
             if let buffer = sampleBuffer {
                 let image = convertSampleBufferToImage(buffer)
-                delegate?.fastCaptureDidCaptureImage(image, fastCapture: self)
+                dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                    self.delegate?.fastCaptureDidCaptureImage(image)
+                })
+
             }
     }
     //https://github.com/gali8/Tesseract-OCR-iOS/wiki/Installation
@@ -88,9 +89,14 @@ class IPFastCapture: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, G8T
             let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
             let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
             let width = CVPixelBufferGetWidth(imageBuffer)
-            let height = CVPixelBufferGetWidth(imageBuffer)
+            let height = CVPixelBufferGetHeight(imageBuffer)
 
             CVPixelBufferUnlockBaseAddress(imageBuffer, 0)
+
+        var info: UInt32 = CGImageAlphaInfo.PremultipliedFirst.rawValue
+            let little: UInt32 = CGBitmapInfo.ByteOrder32Little.rawValue
+
+            info = info | little
 
             let colorSpace = CGColorSpaceCreateDeviceRGB()
             let newContext = CGBitmapContextCreate(baseAddress,
@@ -99,10 +105,10 @@ class IPFastCapture: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, G8T
                 8,
                 bytesPerRow,
                 colorSpace,
-                CGImageAlphaInfo.PremultipliedLast.rawValue)
+                info)
 
             let newImage = CGBitmapContextCreateImage(newContext)
-            let image = UIImage(CGImage: newImage!, scale: 2, orientation: .Right)
+            let image = UIImage(CGImage: newImage!, scale: 1, orientation: .Right)
             return image
         }
         return UIImage()
