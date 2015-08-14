66a594121aa06d6d9a6d47d745a079490b23fa6f
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-08-14 16:49:05 -0400

    quick fix

--- ArtistLookup/ViewController.swift
+++ ArtistLookup/ViewController.swift
@@ -15,21 +15,56 @@ func dispatch_on_main_queue_after(when: dispatch_time_t, block: dispatch_block_t
     })
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, IPFastCaptureDelegate {
 
-    let capture = IPFastCapture()
+    var capture: IPFastCapture?
     let recognizer = ImageRecognizer()
+    @IBOutlet var imageView: UIImageView!
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        capture.delegate = recognizer
-        self.addPreviewLayer()
+        capture = IPFastCapture()
+        capture?.delegate = self
+
+//        self.addPreviewLayer()
+//        recognizer.fastCaptureDidCaptureImage(UIImage(named:"naturebox")!)
+//        recognizer.fastCaptureDidCaptureImage(UIImage(named:"lightning")!)
+//        recognizer.fastCaptureDidCaptureImage(UIImage(named:"three_body")!)
+
+        let updatedFrame = view.frame.scaledRectWithScale(0, yScale: 0.3, widthScale: 1, heightScale: 0.3)
+
+        let debugView  = UIView()
+        imageView.addSubview(debugView)
+
+    }
+
+    func fastCaptureDidCaptureImage(image: UIImage) {
+        imageView.image = image
+        recognizer.fastCaptureDidCaptureImage(image)
+
     }
 
     func addPreviewLayer() {
-        let layer = AVCaptureVideoPreviewLayer(session: self.capture.captureSession)
+        let layer = AVCaptureVideoPreviewLayer(session: capture?.captureSession)
         layer.frame = self.view.bounds
         self.view.layer.addSublayer(layer)
+    }
+}
+
+extension CGRect {
+    func scaledRectWithScale(xScale: CGFloat, yScale: CGFloat widthScale: CGFloat heightScale: CGFloat) -> CGRect {
+        return CGRectMake(origin.x + xScale*size.width,
+            origin.y + yScale*size.height,
+            size.width * widthScale,
+        size.height * heightScale)
     }
 }
 
