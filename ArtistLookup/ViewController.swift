bdeb5b6ad7f21747c2173a779418a9b5cb623dec
Ying Quan Tan <ying.quan.tan@gmail.com>
2015-10-02 15:45:02 -0400

    replaced imageview with av preview layer

--- ArtistLookup/ViewController.swift
+++ ArtistLookup/ViewController.swift
@@ -17,14 +17,15 @@ func dispatch_on_main_queue_after(when: dispatch_time_t, block: dispatch_block_t
 
 class ViewController: UIViewController, IPFastCaptureDelegate {
 
-    let imageSize = CGSizeMake(1146, 1536)
+    let imageSize = CGSizeMake(1080, 1920)
     let croppedSize = CGSizeMake(1000, 400)
 
     var capture: IPFastCapture?
     let recognizer = ImageRecognizer()
+    var hasSetup = false
 
     @IBOutlet weak var outputLabel: UILabel!
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var previewView: UIView!
     var debugView: UIView?
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
@@ -43,16 +44,20 @@ class ViewController: UIViewController, IPFastCaptureDelegate {
 
     override func viewDidLayoutSubviews() {
         // do this in here so that view has proper size by then
-        setUpDebugView()
+        if !hasSetup {
+            hasSetup = true
+            addPreviewLayer()
+            setUpDebugView()
+        }
     }
 
     func setUpDebugView() {
-        let ratio = imageView.frame.width/imageSize.width
+        let ratio = previewView.frame.width/imageSize.width
         debugView?.removeFromSuperview()
 
         debugView = {
             let debugView = UIView()
-            imageView.addSubview(debugView)
+            previewView.addSubview(debugView)
             debugView.autoAlignAxisToSuperviewAxis(.Horizontal)
             debugView.autoAlignAxisToSuperviewAxis(.Vertical)
             debugView.autoSetDimensionsToSize(croppedSize * ratio)
@@ -67,15 +72,14 @@ class ViewController: UIViewController, IPFastCaptureDelegate {
         let croppedImage = resizedImage.cropImageToRect(CGRect(center: CGPointMake(imageSize.width/2, imageSize.height/2),
             width: croppedSize.width,
             height: croppedSize.height))
-        imageView.image = resizedImage
         let text = recognizer.recognizeImage(croppedImage)
         outputLabel.text = text
     }
 
     func addPreviewLayer() {
         let layer = AVCaptureVideoPreviewLayer(session: capture?.captureSession)
-        layer.frame = self.view.bounds
-        self.view.layer.addSublayer(layer)
+        layer.frame = previewView.bounds
+        previewView.layer.insertSublayer(layer, atIndex: 0)
     }
 }
 
